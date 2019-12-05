46cd9873b0b3c8a88c8f5f11de4c8d39ec9b7cc9
Frebin Francis <Frebin.francis@nuvento.net>
2019-12-05 10:13:59 -0600

    first commit

---
+++ sccreens/Inbox.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class Inbox extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Welcome to Inbox Page</Text>
+      </View>
+    );
+  }
+}
+
+export default Inbox;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
